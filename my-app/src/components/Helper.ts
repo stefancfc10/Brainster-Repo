13c1145651f1481294362eb71c7dd9cc5ae51bac
Stefan <stefancfc10@gmail.com>
2024-02-21 14:06:13 +0100

    Completed Challange

---
+++ my-app/src/components/Helper.ts
@@ -0,0 +1,18 @@
+export interface Artist {
+    id: number;
+    name: string;
+    cover: string;
+    bio: string;
+    albums: Album[];
+  }
+
+  export interface Album {
+    
+      albumId: string;
+      title: string;
+      year: number;
+      cover: string;
+      price: number;
+    
+  }
+
